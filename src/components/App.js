e0f2cda1cde19abdf8479eb3eb2da96a770e8ed0
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-12-01 21:03:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,21 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  function hello(e){
+    let x=e.target.value;
+    console.log(e.target.value);
+    if(x.trim===null){
+      document.getElementById('t').innerText=`1`;
+    }else{
+      document.getElementById('t').innerText=`Hello ${x}`;
+    }
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <label>Enter your name:</label>
+        <br/>
+        <input onChange={hello}/>
+        <p id='t'></p>
     </div>
   )
 }
